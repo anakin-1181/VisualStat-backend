7fd7e907fc56087404f510b5ca7bebbd78ab04e5
Anakin Lai <anakin252lai@gmail.com>
2025-06-15 07:42:59 +0800

    pydantic schema and figure to image update

---
+++ app/utils/image.py
@@ -0,0 +1,17 @@
+import io
+from fastapi.responses import StreamingResponse
+import matplotlib.pyplot as plt
+
+def fig_to_image(fig):
+    # Create an buffer to store image
+    buf = io.BytesIO() 
+    # Save fig to buffer and free up memory (?)
+    fig.savefig(buf, format="png")
+    plt.close(fig)
+    # Reset and read the buffer
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
+    
+
+
+ 
